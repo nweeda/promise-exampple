309cdcb6e356668f0437b79282e8e2c470616b66
Nick <nicholas.weeda@gmail.com>
2022-12-02 08:42:30 -0800

    resolved/rejected

--- promises.js
+++ promises.js
@@ -1,6 +1,6 @@
 //promises
 //commit to something (ie i promise to...), then 2 results (completed -> resolved, or failed -> rejected
-
+console.log("hi");
 let p = new Promise((resolve, reject) => {
   let a = 1 + 1;
   if (a == 2) {
@@ -8,4 +8,11 @@ let p = new Promise((resolve, reject) => {
   } else {
     reject("Failed");
   }
+});
+
+//if resolved -> then, catch -> rejected
+p.then((message) => {
+  console.log("This is in the then " + message);
+}).catch((message) => {
+  console.log("This is in the catch " + message);
 });
